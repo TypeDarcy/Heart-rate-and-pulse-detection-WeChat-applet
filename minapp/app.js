55925cfb391075ab282b8ce74ca6d92d647dae10
TypeDarcy <86557881+TypeDarcy@users.noreply.github.com>
2022-11-19 21:11:11 +0800

    Update app.js

--- minapp/app.js
+++ minapp/app.js
@@ -13,7 +13,7 @@ App({
   onLaunch() {
     //隐藏系统tabBar
     wx.hideTabBar();
-    this.globalData.client = mqtt.connect('wxs://www.codingroad.top:8084/mqtt')
+    this.globalData.client = mqtt.connect('wxs://mqtt服务器地址8084/mqtt')
     this.globalData.client.on('connect', (e) => {
       console.log('服务器连接成功')
       this.globalData.client.subscribe('/mqtt/testtopic', function (err) {
